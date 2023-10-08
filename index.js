63356ca42b4ca375bae8ae480fa3c922797fec8e
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-10-08 22:42:03 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+// write a program to add two numbers
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(10, 20));
+// String Initialization
+let Name = "Usman";
+console.log(Name);
+// String Concatenation
+let firstName = "Usman";
+let lastName = "Saeed";
+let fullName = firstName + lastName;
+console.log("FullName is :", fullName);
+//String Length
+let myName = "Usman Saeed";
+console.log("Length of Usman Saeed is :", myName.length);
+// todo Uppercase and Lowercase
+let case1 = "Usman Saeed";
+console.log(case1.toUpperCase());
+console.log(case1.toLowerCase());
---
+++ index.ts
@@ -0,0 +1,24 @@
+// write a program to add two numbers
+function sum(a:number , b:number) {
+    return a+b
+}
+console.log(sum(10,20))
+
+// String Initialization
+let Name:string = "Usman"
+console.log(Name)
+
+// String Concatenation
+let firstName:string = "Usman"
+let lastName:string = "Saeed"
+let fullName:string = firstName + lastName
+console.log("FullName is :",fullName )
+
+//String Length
+let myName:string = "Usman Saeed"
+console.log("Length of Usman Saeed is :",myName.length)
+
+// todo Uppercase and Lowercase
+let case1:string = "Usman Saeed"
+console.log(case1.toUpperCase())
+console.log(case1.toLowerCase())
